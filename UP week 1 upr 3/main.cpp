37ae4e1f4991bf623a62ad39b56af2f11166553c
Keynel12 <stefan1295@hotmail.com>
2014-11-20 14:38:29 +0200

    Tasks
    Tasks

---
+++ UP week 1 upr 3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    bool lovePrograming = true;
+    bool loveLudogorets = false;
+    // I don't hate them either!
+    bool programShouldHaveNoComments = false;
+    std::cout << "Love programing? " << std::boolalpha << lovePrograming << std::endl;
+    std::cout << "Love Ludogorets? " << std::boolalpha << loveLudogorets << std::endl;
+    std::cout << "Programs sould have no comments? " << std::boolalpha << programShouldHaveNoComments << std::endl;
+    return 0;
+}
